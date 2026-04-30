d8a6dad694da045ac4ec94ceb4e01ba37ab93d83
Tim <timbraukmann@gmx.de>
2026-04-30 11:27:35 +0200

    Bugfixes im inheritance.cpp

--- courses/coding-in-C++/Loesungenmy/Lab3/inheritance.cpp
+++ courses/coding-in-C++/Loesungenmy/Lab3/inheritance.cpp
@@ -1,42 +1,43 @@
 #include <iostream>
 #include <string>
 
-class User
-{
-    protected: 
+class User {
+    protected:
         int id;
         std::string name;
     public:
-        User(std::string name, int id);
+        User(std::string name, int id) : name(name), id(id) {}  
         void printInfo();
 };
 
-class Student : protected User 
-{
-    private: 
+class Student : public User {
+    private:
         int year;
     public:
-        void printInfo()
-        {
-            std::cout << name, id, year  << std::endl;
+        Student(std::string name, int id, int year)  
+            : User(name, id), year(year) {}
+        
+        void printInfo() {
+            std::cout << name << " " << id << " " << year << std::endl;  
         }
-
 };
 
-class Instructor : protected User
-{
-    private: 
+class Instructor : public User {
+    private:
         std::string profession;
-
     public:
-        void printInfo()
-        {
-            std::cout << name, id, profession << std::endl;
+        Instructor(std::string name, int id, std::string profession)  
+            : User(name, id), profession(profession) {}
+        
+        void printInfo() {
+            std::cout << name << " " << id << " " << profession << std::endl;  
         }
-
 };
 
-int main()
-{
+int main() {
+    Student tim("tim", 12, 8);       
+    Instructor john("john", 23, "math"); 
+    tim.printInfo();                 
+    john.printInfo();
     return 0;
 }
